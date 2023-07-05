789b23a5cfe875de5a8f5b72eaf6676b8102226f
Ruben Pingol <ruben.pingol@xibosignage.com>
2023-07-05 14:24:20 +0800

    Elements: Currencies add XiboImageRender resolvex xibosignageltd/xibo-private#302

--- ui/bundle_tools.js
+++ ui/bundle_tools.js
@@ -1,3 +1,6 @@
 // --- Add global TOOLS for the CMS ----
 window.formHelpers = require('./src/helpers/form-helpers.js');
 window.transformer = require('./src/helpers/transformer.js');
+
+// Our own imports
+import '../modules/src/xibo-image-render.js';
